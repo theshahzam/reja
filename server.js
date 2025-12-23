4931d5c4a6ef7ada0b22755ef2a1de682f7ac3d9
theshahzam <theshahzam@gmail.com>
2025-12-24 00:45:46 +0900

    feat: harid.ejs added

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ const http = require("http")
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 //2 SESSION
 //3 VIEWS CODE
 app.set("views", "views");
 app.set("view engine", "ejs");
+
 //4 ROUTING
-app.get("/", function (req, res) {
-    res.end("<h1 style=\"background: red; color: white;\">Server by NOAH</h1>");
+app.post("/create-item", function (req, res) {
+    console.log(req.body);
+    res.json({test: "Success" });
 });
-app.get("/gift", function (req, res) {
-    res.end("<h1 style=\"background: red; color: white;\">1-page by NOAH</h1>");
+
+app.get("/", function (req, res) { 
+    res.render("harid", { pageTitle: "Home" });
 });
 
 const server = http.createServer(app);
